a270cb1003b65751120d4b6d74213e9706f6b4e4
Michael Shannon <michaelrshannon@gmail.com>
2013-07-27 21:11:03 +0100

    bugfix in next api call

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -3,7 +3,9 @@ class StatementsController < ApplicationController
   # GET /tweets.json
   def show
     session[:NEXT_ID] = Statement.first.id - 1 if params[:position] == :first
+
     begin
+      raise ActiveRecord::RecordNotFound unless session[:NEXT_ID]
       session[:NEXT_ID] = session[:NEXT_ID] + 1
       @statement = Statement.find(session[:NEXT_ID])
     rescue ActiveRecord::RecordNotFound
