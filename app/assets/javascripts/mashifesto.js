ebb2fedcc6da9c1a1cfde53983abc4b5fcd89da6
Alasdair North <alnorth@gmail.com>
2013-07-28 10:08:45 +0100

    Animating the "kevin" template.

--- app/assets/javascripts/mashifesto.js
+++ app/assets/javascripts/mashifesto.js
@@ -27,7 +27,7 @@ $(function () {
 			TweenMax.from('#stmt-3', 1, {left: '2000px', delay: 4.5});
 			TweenMax.from('#handle', 1, {left: '2000px', delay: 4.75});
 
-			TweenMax.to('.mashifesto', 2, {top: '2546px', left: '1721px', delay: 8.5});
+			TweenMax.to('.mashifesto', 2, {top: '2456px', left: '1721px', delay: 8.5});
 
 		} else if(templateName === 'michael') {
 
@@ -49,7 +49,30 @@ $(function () {
 			TweenMax.from('#handle', 1, {left: '2000px', delay: 5.75});
 			TweenMax.from('#mashifesto-handle', 1, {left: '2000px', delay: 6});
 
-			TweenMax.to('.mashifesto', 2, {top: '-2546px', left: '-1721px', delay: 9});
+			TweenMax.to('.mashifesto', 2, {top: '-2456px', left: '-1721px', delay: 9});
+
+		} else if(templateName === 'kevin') {
+
+			TweenMax.to('.mashifesto', 0, {top: '0px', left: '0px'});
+			
+			TweenMax.from('#stmt-0', 1, {bottom: '-2000px'});
+			TweenMax.from('#block-0', 1, {bottom: '-2000px', delay: 0.25});
+
+			TweenMax.from('#stmt-1', 1, {bottom: '-2000px', delay: 2.25});
+			TweenMax.from('#block-1', 1, {right: '-2000px', delay: 2.5});
+			TweenMax.from('#left-block', 1, {right: '-2000px', delay: 2.75});			
+			TweenMax.from('#block-3', 1, {right: '-2000px', delay: 2.75});
+
+			TweenMax.from('#hands-left', 1, {'background-position-y': '650%', delay: 3.5});
+			TweenMax.from('#hands-right', 1, {'background-position-y': '650%', delay: 3.5});
+
+			TweenMax.from('#stmt-2', 1, {top: '2000px', delay: 4.5});
+
+			TweenMax.from('#stmt-3', 1, {top: '2000px', delay: 5});
+			TweenMax.from('#handle', 1, {left: '2000px', delay: 5.5});
+			TweenMax.from('#mashifesto-handle', 1, {left: '2000px', delay: 5.75});
+			
+			TweenMax.to('.mashifesto', 2, {top: '-2456px', left: '-1721px', delay: 9});
 		}
 	}
 
